3f3d80cacaf944f935036e4eaf41e9fa814bb82d
Pliffdax <stepanov-oleksandr@ukr.net>
2025-05-10 15:10:15 +0300

    Vitest addition to test software

--- src/tests/testEnv.ts
+++
@@ -1,3 +0,0 @@
-import { JWT_SECRET } from "@/config/env";
-
-console.log(`Token ${JWT_SECRET}`)
---
+++ vitest.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vitest/config'
+import * as path from 'path'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'node',
+    coverage: {
+      provider: "v8",
+      reporter: ['text', 'html'],
+      exclude: [
+        'src/generated/**',
+        'prisma/**',
+        'tests/**',
+        'src/routes/**',
+        'src/controllers/**',
+        'src/middleware/**',
+        'src/utils/**',
+        'src/validators/**',
+        'src/config/**',
+        'src/services/auth.service.ts',
+        'src/services/auth.service.ts',
+        'src/server.ts',
+        'vitest.config.ts',
+      ],
+    },
+  },
+})
